6fc9742955b34d756ac892f7d1e789afdd4493a5
Aous Deeb <abomarselsyr@gmail.com>
2025-04-21 02:49:05 +0200

    [DEV] add whichkey plugin

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -1,4 +1,22 @@
 return {
-    {'folke/tokyonight.nvim'},
-    {"scottmckendry/cyberdream.nvim"},
-   }
+	{'folke/tokyonight.nvim'},
+	{"scottmckendry/cyberdream.nvim"},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	}
+}
--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,29 +1,53 @@
 return {
-    "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+	tag = "0.1.8",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+	},
 
-    config = function()
-        require('telescope').setup({})
-
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-    end
+	config = function()
+		local actions = require("telescope.actions")
+		require('telescope').setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i={
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-c>"] = actions.close,
+					},
+					n={},
+				}
+			},
+			pickers = {
+				find_files = {
+					theme = "ivy"
+				}
+			},
+			extentions = {
+				fzf = {}
+			}
+		})
+		require('telescope').load_extension('fzf')
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+		vim.keymap.set('n', '<leader>pws', function()
+			local word = vim.fn.expand("<cword>")
+			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set('n', '<leader>pWs', function()
+			local word = vim.fn.expand("<cWORD>")
+			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set('n', '<leader>ps', function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
+		vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+		-- to resume the last search
+		-- vim.keymap.set('n', '<leader>fr', builtin.resume, {})
+	end
 }
