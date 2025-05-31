88f31b9a4ed91c1b1479b8b252613d947e3f1352
Aous Deeb <abomarselsyr@gmail.com>
2025-05-31 21:47:29 +0200

    added lua lsp

---
+++ lua/config/lsp/lua_lsp.lua
@@ -0,0 +1,14 @@
+-- Lua
+vim.lsp.config.lua_ls = {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { ".luarc.json", ".git", vim.uv.cwd() },
+  settings = {
+    Lua = {
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+vim.lsp.enable("lua_ls")
