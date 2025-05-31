0c26f6047c10bfaf406ba0091fb4dd8481843f56
Aous Deeb <abomarselsyr@gmail.com>
2025-05-31 21:47:29 +0200

    added lua lsp

--- lua/config/plugins/lsp_config.lua
+++ lua/config/plugins/lsp_config.lua
@@ -1,20 +1,20 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
+	-- "neovim/nvim-lspconfig",
+	-- dependencies = {
 		"stevearc/conform.nvim",
 		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
+--		"williamboman/mason-lspconfig.nvim",
+	-- 	"hrsh7th/cmp-nvim-lsp",
+	-- 	"hrsh7th/cmp-buffer",
+	-- 	"hrsh7th/cmp-path",
+	-- 	"hrsh7th/cmp-cmdline",
+	-- 	"hrsh7th/nvim-cmp",
 		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
+		-- "saadparwaiz1/cmp_luasnip",
 		"j-hui/fidget.nvim",
-		"rshkarin/mason-nvim-lint",
+--		"rshkarin/mason-nvim-lint",
 
-	},
+	-- },
 	config = function()
 		require('mason').setup({
 			ui = {
@@ -26,60 +26,36 @@ return {
 			},
 		})
 
-		require('mason-lspconfig').setup({
-			ensure_installed = {
-				'sonarlint-language-server',
-				'lua_ls',
-				'gopls',
-				'csharp_ls',
-				'omnisharp',
-				'ts_ls',
-				'eslint',
-				'angularls',
-				'superhtml',
-				'lemminx'
-			},
-			handlers = {
-				function(server_name)
-					require('lspconfig')[server_name].setup({})
-				end,
-			}
-		})
-		vim.diagnostic.config({
-			virtual_lines = {
-				-- Only show virtual line diagnostics for the current cursor line
-				current_line = true,
-			},
-		})
-
+-- 		require('mason-lspconfig').setup({
+-- 			ensure_installed = {
+-- 				'sonarlint-language-server',
+-- 				'lua_ls',
+-- 				'gopls',
+-- 				'csharp_ls',
+-- 				'omnisharp',
+-- 				'ts_ls',
+-- 				'eslint',
+-- 				'angularls',
+-- 				'superhtml',
+-- 				'lemminx'
+-- 			},
+-- 			handlers = {
+-- 				function(server_name)
+-- 					require('lspconfig')[server_name].setup({})
+-- 				end,
+-- 			}
+-- 		})
 		-- Add cmp_nvim_lsp capabilities settings to lspconfig
 		-- This should be executed before you configure any language server
-		local lspconfig_defaults = require('lspconfig').util.default_config
-		lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-			'force',
-			lspconfig_defaults.capabilities,
-			require('cmp_nvim_lsp').default_capabilities()
-		)
+		-- local lspconfig_defaults = require('lspconfig').util.default_config
+	-- 	lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+	-- 		'force',
+	-- 		lspconfig_defaults.capabilities,
+	-- 		require('cmp_nvim_lsp').default_capabilities()
+	-- 	)
 
 		-- This is where you enable features that only work
 		-- if there is a language server active in the file
-		vim.api.nvim_create_autocmd('LspAttach', {
-			desc = 'LSP actions',
-			callback = function(event)
-				local opts = { buffer = event.buf }
-
-				vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-				vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-				vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-				vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-				vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-				vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-				vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-				vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-				vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-				vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-			end,
-		})
 
 		require('mason-nvim-lint').setup({
 			--ensure_installed = { 'ast-grep', 'prettier'},
@@ -92,63 +68,63 @@ return {
 		luasnip.config.setup {}
 
 
-		local cmp = require('cmp')
-
-		cmp.setup({
-
-			snippet = {
-				expand = function(args)
-					vim.snippet.expand(args.body)
-				end,
-			},
-
-			mapping = cmp.mapping.preset.insert({
-				-- Navigate between completion items
-				['<C-k>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
-				['<C-j>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
-				-- `Enter` key to confirm completion
-				['<CR>'] = cmp.mapping.confirm({ select = false }),
-				-- Ctrl+Space to trigger completion menu
-				['<C-Space>'] = cmp.mapping.complete(),
-
-				-- Scroll up and down in the completion documentation
-				['<C-u>'] = cmp.mapping.scroll_docs(-4),
-				['<C-d>'] = cmp.mapping.scroll_docs(4),
-				['<Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { 'i', 's' }),
-				['<S-Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { 'i', 's' }),
-			}),
-			sources = cmp.config.sources({
-				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' },
-			}),
-		})
-		vim.diagnostic.config({
-			-- update_in_insert = true,
-			float = {
-				focusable = false,
-				style = "minimal",
-				border = "rounded",
-				source = "always",
-				header = "",
-				prefix = "",
-			},
-		})
-	end
+-- 		local cmp = require('cmp')
+-- 
+-- 		cmp.setup({
+-- 
+-- 			snippet = {
+-- 				expand = function(args)
+-- 					vim.snippet.expand(args.body)
+-- 				end,
+-- 			},
+-- 
+-- 			mapping = cmp.mapping.preset.insert({
+-- 				-- Navigate between completion items
+-- 				['<C-k>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+-- 				['<C-j>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+-- 				-- `Enter` key to confirm completion
+-- 				['<CR>'] = cmp.mapping.confirm({ select = false }),
+-- 				-- Ctrl+Space to trigger completion menu
+-- 				['<C-Space>'] = cmp.mapping.complete(),
+-- 
+-- 				-- Scroll up and down in the completion documentation
+-- 				['<C-u>'] = cmp.mapping.scroll_docs(-4),
+-- 				['<C-d>'] = cmp.mapping.scroll_docs(4),
+-- 				['<Tab>'] = cmp.mapping(function(fallback)
+-- 					if cmp.visible() then
+-- 						cmp.select_next_item()
+-- 					elseif luasnip.expand_or_jumpable() then
+-- 						luasnip.expand_or_jump()
+-- 					else
+-- 						fallback()
+-- 					end
+-- 				end, { 'i', 's' }),
+-- 				['<S-Tab>'] = cmp.mapping(function(fallback)
+-- 					if cmp.visible() then
+-- 						cmp.select_prev_item()
+-- 					elseif luasnip.jumpable(-1) then
+-- 						luasnip.jump(-1)
+-- 					else
+-- 						fallback()
+-- 					end
+-- 				end, { 'i', 's' }),
+-- 			}),
+-- 			sources = cmp.config.sources({
+-- 				{ name = 'nvim_lsp' },
+-- 				{ name = 'luasnip' },
+-- 			}),
+-- 		})
+-- 		vim.diagnostic.config({
+-- 			-- update_in_insert = true,
+-- 			float = {
+-- 				focusable = false,
+-- 				style = "minimal",
+-- 				border = "rounded",
+-- 				source = "always",
+-- 				header = "",
+-- 				prefix = "",
+-- 			},
+-- 		})
+ 	end
 
 }
