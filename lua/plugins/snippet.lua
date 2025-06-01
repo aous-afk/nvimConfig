e1cba10852b17665cca4e82659a1f31bf848df51
Aous Deeb <abomarselsyr@gmail.com>
2025-06-01 22:16:51 +0200

    [DEV] added nvim lint and snippet

--- lua/plugins/snippet.lua
+++ lua/plugins/snippet.lua
@@ -10,16 +10,38 @@ return {
 	},
 
 	config = function()
-		local ls = require("luasnip")
-		local s = ls.snippet
-		local t = ls.text_node
-		local i = ls.insert_node
-		local extras = require("luasnip.extras")
-		local rep = extras.rep
-		local fmt = require("luasnip.extras.fmt").fmt
-		local c = ls.choice_node
+	    local ls = require("luasnip")
+	    local s = ls.snippet
+	    local t = ls.text_node
+	    local i = ls.insert_node
+	    local extras = require("luasnip.extras")
+	    local rep = extras.rep
+	    local fmt = require("luasnip.extras.fmt").fmt
+	    local c = ls.choice_node
 		require("luasnip.loaders.from_vscode").lazy_load()
 		require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
+		-- Yes, we're just executing a bunch of Vimscript, but this is the officially
+-- endorsed method; see https://github.com/L3MON4D3/LuaSnip#keymaps
+vim.cmd[[
+" Use Tab to expand and jump through snippets
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+" Use Shift-Tab to jump backwards through snippets
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]]
+	
+		ls.config.set_config({ -- Setting LuaSnip config
+
+			-- Enable autotriggered snippets
+			enable_autosnippets = true,
+
+			-- Use Tab (or some other key if you prefer) to trigger visual selection
+			store_selection_keys = "<Tab>",
+		})
+
+	    ls.filetype_extend("javascript", { "jsdoc" })
 
 		vim.keymap.set({"i"}, "<C-K>",
 		function()
