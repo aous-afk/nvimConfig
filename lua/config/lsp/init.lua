1cb77f9047e9fc998025612c29760266816cc5bc
Aous Deeb <abomarselsyr@gmail.com>
2025-06-01 17:02:45 +0200

    changed the structure

--- lua/config/lsp/init.lua
+++
@@ -1,9 +0,0 @@
-require("config.lsp.lua_lsp")
-require("config.lsp.csharp_lsp")
-require("config.lsp.cmd_nvim")
-vim.diagnostic.config({
-	virtual_lines = {
-		-- Only show virtual line diagnostics for the current cursor line
-		current_line = true,
-	},
-})
