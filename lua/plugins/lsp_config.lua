646841716b09db29e24db954faa759eafe9f13dc
aous-afk <abomarselsyr@gmail.com>
2025-06-02 12:44:42 +0200

    added cmp

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -5,15 +5,6 @@ return {
 		"williamboman/mason.nvim",
 		"j-hui/fidget.nvim",
 		"Decodetalkers/csharpls-extended-lsp.nvim",
-		--	"williamboman/mason-lspconfig.nvim",
-		-- 	"hrsh7th/cmp-nvim-lsp",
-		-- 	"hrsh7th/cmp-buffer",
-		-- 	"hrsh7th/cmp-path",
-		-- 	"hrsh7th/cmp-cmdline",
-		-- 	"hrsh7th/nvim-cmp",
-		-- "saadparwaiz1/cmp_luasnip",
-		--	"rshkarin/mason-nvim-lint",
-
 	},
 	config = function()
 		require('mason').setup({
@@ -25,112 +16,17 @@ return {
 				},
 			},
 		})
+		local capabilities = require('cmp_nvim_lsp').default_capabilities()
 		local config = {
 			handlers = {
 				["textDocument/definition"] = require('csharpls_extended').handler,
 				["textDocument/typeDefinition"] = require('csharpls_extended').handler,
 			},
 			cmd = { "csharp-ls" },
-			-- rest of your settings
+            capabilities = capabilities,
 		}
 		require'lspconfig'.csharp_ls.setup(config)
 		require("csharpls_extended").buf_read_cmd_bind()
 		require("fidget").setup({})
-
-
-
-
--- 		require('mason-lspconfig').setup({
--- 			ensure_installed = {
--- 				'sonarlint-language-server',
--- 				'lua_ls',
--- 				'gopls',
--- 				'csharp_ls',
--- 				'omnisharp',
--- 				'ts_ls',
--- 				'eslint',
--- 				'angularls',
--- 				'superhtml',
--- 				'lemminx'
--- 			},
--- 			handlers = {
--- 				function(server_name)
--- 					require('lspconfig')[server_name].setup({})
--- 				end,
--- 			}
--- 		})
-		-- Add cmp_nvim_lsp capabilities settings to lspconfig
-		-- This should be executed before you configure any language server
-		-- local lspconfig_defaults = require('lspconfig').util.default_config
-	-- 	lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-	-- 		'force',
-	-- 		lspconfig_defaults.capabilities,
-	-- 		require('cmp_nvim_lsp').default_capabilities()
-	-- 	)
-
-		-- This is where you enable features that only work
-		-- if there is a language server active in the file
-
--- 		require('mason-nvim-lint').setup({
--- 			--ensure_installed = { 'ast-grep', 'prettier'},
--- 		})
-
--- 		local cmp = require('cmp')
--- 
--- 		cmp.setup({
--- 
--- 			snippet = {
--- 				expand = function(args)
--- 					vim.snippet.expand(args.body)
--- 				end,
--- 			},
--- 
--- 			mapping = cmp.mapping.preset.insert({
--- 				-- Navigate between completion items
--- 				['<C-k>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
--- 				['<C-j>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
--- 				-- `Enter` key to confirm completion
--- 				['<CR>'] = cmp.mapping.confirm({ select = false }),
--- 				-- Ctrl+Space to trigger completion menu
--- 				['<C-Space>'] = cmp.mapping.complete(),
--- 
--- 				-- Scroll up and down in the completion documentation
--- 				['<C-u>'] = cmp.mapping.scroll_docs(-4),
--- 				['<C-d>'] = cmp.mapping.scroll_docs(4),
--- 				['<Tab>'] = cmp.mapping(function(fallback)
--- 					if cmp.visible() then
--- 						cmp.select_next_item()
--- 					elseif luasnip.expand_or_jumpable() then
--- 						luasnip.expand_or_jump()
--- 					else
--- 						fallback()
--- 					end
--- 				end, { 'i', 's' }),
--- 				['<S-Tab>'] = cmp.mapping(function(fallback)
--- 					if cmp.visible() then
--- 						cmp.select_prev_item()
--- 					elseif luasnip.jumpable(-1) then
--- 						luasnip.jump(-1)
--- 					else
--- 						fallback()
--- 					end
--- 				end, { 'i', 's' }),
--- 			}),
--- 			sources = cmp.config.sources({
--- 				{ name = 'nvim_lsp' },
--- 				{ name = 'luasnip' },
--- 			}),
--- 		})
--- 		vim.diagnostic.config({
--- 			-- update_in_insert = true,
--- 			float = {
--- 				focusable = false,
--- 				style = "minimal",
--- 				border = "rounded",
--- 				source = "always",
--- 				header = "",
--- 				prefix = "",
--- 			},
--- 		})
 	end
 }
