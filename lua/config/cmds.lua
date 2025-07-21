ce73ea0a534e0d55c63f613ceddfae536bcfb90e
aous-afk <abomarselsyr@gmail.com>
2025-07-21 11:07:02 +0200

    [DEV] added config for work laptop

--- lua/config/cmds.lua
+++ lua/config/cmds.lua
@@ -75,3 +75,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 
 })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "NeogitStatus",
+  callback = function()
+    vim.wo.number = true
+    vim.wo.relativenumber = true
+  end,
+})
